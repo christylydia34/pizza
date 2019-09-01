09ae85d953f3a7d0bdb4eb7e5f1aae206fe73341
christylydia <christinegikandi@gmail.com>
2019-09-01 13:36:48 +0300

    add js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+function Order(type,size,crust,topping,number){
+    this.type=type;
+    this.size=size;
+    this.topping=topping;
+    this.number=number;
+}
+
+
+
 $(document).ready (function(){
     $("#he1").click(function(){
         $(".pizzol").toggle(500);
